e81769cbc7ca2354f7906b46dd682078174dd9ad
Alexey <forhotos@gmail.com>
2021-09-29 23:25:50 +0400

    Sharing

--- src/components/card-list/cardList.js
+++ src/components/card-list/cardList.js
@@ -1,6 +1,7 @@
 import React, {useCallback, useEffect, useState} from "react";
 import axios from "axios";
 import Modal from "react-bootstrap/Modal";
+import {DropdownButton, Dropdown} from "react-bootstrap";
 
 export default function CardList(props) {
     const {pics} = props;
@@ -75,8 +76,8 @@ export default function CardList(props) {
                                          alt={pic.alt_description}
                                          src={pic.urls.regular}
                                     />,
-                                triggerDownload: pic.links.download_location,
-                                download: pic.urls.full
+                                download: pic.urls.full,
+                                share: pic.links.html
                             }
                         );
                         setShow(true);
@@ -197,9 +198,60 @@ export default function CardList(props) {
                     {modalPhoto.img}
                 </Modal.Body>
                 <Modal.Footer>
-                    <button type="button" id="button-view-share-modal">
-                        Share
-                    </button>
+                    <DropdownButton
+                        variant="outline-secondary"
+                        align="start"
+                        id="button-view-share-modal"
+                        onClick={() => setShare(true)}
+                        title={
+                            <>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor"
+                                     className="share-button-icon" viewBox="0 0 16 16">
+                                    <path
+                                        d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+                                </svg>
+                                Share
+                            </>
+                        }
+                        drop="up"
+                    >
+                        <Dropdown.Item
+                            eventKey="1"
+                            className="share-item"
+                            onClick={() => {
+                                const text = encodeURIComponent("What a beautiful picture!");
+                                const hash_tags = "Beautiful,Photo,Picture,Gallery";
+                                const shareUrl = `https://twitter.com/intent/tweet?url=${modalPhoto.share}&text=${text}&hashtags=${hash_tags}`;
+                                window.open(shareUrl,"_blank" ).focus();
+                            }}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="rgb(29, 155, 240)"
+                                 className="share-item-icon" viewBox="0 0 16 16">
+                                <path
+                                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                            </svg>
+                            <span className="share-item-label">
+                                Twitter
+                            </span>
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            eventKey="2"
+                            className="share-item"
+                            onClick={() => {
+                                const shareUrl = `http://www.facebook.com/sharer/sharer.php?u=${modalPhoto.share}`;
+                                window.open(shareUrl,"_blank" ).focus();
+                            }}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#365899"
+                                 className="share-item-icon" viewBox="0 0 16 16">
+                                <path
+                                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                            </svg>
+                            <span className="share-item-label">
+                                Facebook
+                            </span>
+                        </Dropdown.Item>
+                    </DropdownButton>
                 </Modal.Footer>
             </Modal>
             }
